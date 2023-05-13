b6235e1f21e4a65cf935ed69bd0a6a2ce1dc288a
NanakoOfficial <712111161@qq.com>
2023-05-14 00:30:33 +0800

    Update __init__.py

--- nonebot_plugin_githubcard/__init__.py
+++ nonebot_plugin_githubcard/__init__.py
@@ -4,17 +4,15 @@ from nonebot.adapters.onebot.v11 import Bot, GroupMessageEvent, Message
 import re
 from .config import Config
 from .data_source import get_github_reposity_information
-
+from nonebot.plugin import on_regex
 
 global_config = get_driver().config
 config = Config(**global_config.dict())
-github = on_message(priority=50,block=False)
-
+github = on_regex(r"github.com\/(.+)?\/([^\/\s]+)", priority=10, block=False)
 
 @github.handle()
 async def github_handle(bot: Bot, event: GroupMessageEvent, state: T_State):
     url = event.get_plaintext()
-    if re.match("https://github.com/.*?/.*?", url) != None:
-        imageUrl = await get_github_reposity_information(url)
-        assert(imageUrl != "获取信息失败")
-        await github.send(Message(f"[CQ:image,file={imageUrl}]"))
+    imageUrl = await get_github_reposity_information(url)
+    assert(imageUrl != "获取信息失败")
+    await github.send(Message(f"[CQ:image,file={imageUrl}]"))
