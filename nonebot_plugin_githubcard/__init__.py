cee55a887072c139036f38bceb0260be088aaaef
Night-stars-1 <99261160+Night-stars-1@users.noreply.github.com>
2023-06-04 16:44:30 +0800

    Add files via upload

--- nonebot_plugin_githubcard/__init__.py
+++ nonebot_plugin_githubcard/__init__.py
@@ -1,17 +1,39 @@
+'''
+Author: Night-stars-1 nujj1042633805@gmail.com
+Date: 2023-06-04 16:33:00
+LastEditors: Night-stars-1 nujj1042633805@gmail.com
+LastEditTime: 2023-06-04 16:39:24
+Description: 
+
+Copyright (c) 2023 by Night-stars-1, All Rights Reserved. 
+'''
 from nonebot.rule import T_State
 from nonebot import get_driver
+from nonebot.log import logger
 from nonebot.adapters.onebot.v11 import Bot, GroupMessageEvent, MessageSegment
 from .config import Config
 from .data_source import get_github_reposity_information
 from nonebot.plugin import on_regex
 
+import re
+
 global_config = get_driver().config
 config = Config(**global_config.dict())
-github = on_regex(r"github.com\/(.+)?\/([^\/\s]+)", priority=10, block=False)
+github = on_regex(r"https?://github\.com/([^/]+/[^/]+)", priority=10, block=False)
 
+def match_link_parts(link):
+    pattern = r'https?://github\.com/([^/]+/[^/]+)'
+    match = re.search(pattern, link)
+    if match:
+        return match.group(0)
+    else:
+        return None
+    
 @github.handle()
 async def github_handle(bot: Bot, event: GroupMessageEvent, state: T_State):
-    url = event.get_plaintext()
+    url = match_link_parts(event.get_plaintext())
+    logger.info(url)
     imageUrl = await get_github_reposity_information(url)
     assert(imageUrl != "获取信息失败")
     await github.send(MessageSegment.image(imageUrl))
+    
