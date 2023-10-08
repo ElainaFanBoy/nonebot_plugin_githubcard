ca8e6268712ce91c6dbbfd4f8ac7226a5b75eeeb
NanakoOfficial <712111161@qq.com>
2023-10-08 19:14:43 +0800

    update

--- nonebot_plugin_githubcard/__init__.py
+++ nonebot_plugin_githubcard/__init__.py
@@ -18,7 +18,7 @@ __plugin_meta__ = PluginMetadata(
     extra={
         "unique_name": "githubcard",
         "author": "Nanako <demo0929@vip.qq.com>",
-        "version": "0.1.7",
+        "version": "0.1.8",
     },
 )
 
