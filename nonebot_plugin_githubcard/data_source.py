f67f436c3f84fb541f8f751476ae9f984054f083
Nanako <56375835+ElainaFanBoy@users.noreply.github.com>
2024-03-13 09:54:26 +0800

    Update data_source.py

--- nonebot_plugin_githubcard/data_source.py
+++ nonebot_plugin_githubcard/data_source.py
@@ -33,7 +33,7 @@ async def get_github_reposity_information(url: str) -> str:
             RawData = await response.json()
             AvatarUrl = RawData["avatar_url"]
             if github_type == 0:
-                ImageUrl = f"https://image.thum.io/get/width/1280/crop/640/viewportWidth/1280/png/noanimate/https://socialify.git.ci/{UserName}/{RepoName}/image?description=1&font=Rokkitt&forks=1&issues=1&language=1&name=1&owner=1&pattern=Circuit%20Board&pulls=1&stargazers=1&theme=Light&logo={AvatarUrl}"
+                ImageUrl = f"https://socialify.git.ci/{UserName}/{RepoName}/png?description=1&font=Rokkitt&forks=1&issues=1&language=1&name=1&owner=1&pattern=Circuit%20Board&pulls=1&stargazers=1&theme=Light&logo={AvatarUrl}"
             else:
                 ImageUrl = f"https://opengraph.githubassets.com/githubcard/{UserName}/{RepoName}"
             return ImageUrl
